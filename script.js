1054e0207dc5261da3d3e7110bb15f51e9e29304
Anthonybhn <bustamantenumb@gmail.com>
2025-06-13 19:18:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("matrixCanvas");
 const ctx = canvas.getContext("2d");
 let speed = 10;
-let message = "Te Amo 💘";
+let message = "Te Amo mi princesa 💘";
 let color = "#ffffff";
 
 canvas.width = window.innerWidth;
